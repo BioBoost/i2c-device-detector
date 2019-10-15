94ed32538c6674bb0c1b9d90aa815cbb9afc08a0
Nico De Witte <nico.dewitte@vives.be>
2019-10-15 12:45:41 +0100

    Dont scan when creating overview

--- src/i2c_device_detector.js
+++ src/i2c_device_detector.js
@@ -5,23 +5,23 @@ class I2cDetector {
   constructor(i2c, deviceDescriptors) {
     this.i2c = i2c;
     this.deviceDescriptors = deviceDescriptors;
+    this.devices = [];
   }
 
   scan() {
-    let devices = this.i2c.scanSync();
-    devices = devices.map( address => `0x${address.toString(16)}`).map(
+    this.devices = this.i2c.scanSync();
+    this.devices = this.devices.map( address => `0x${address.toString(16)}`).map(
       address => [
         address,
         this.deviceDescriptors[address] ? this.deviceDescriptors[address].description : 'unknown'
       ]
     );
-    return devices;
+    this.devices.unshift(['I2C Address', 'Description']);
+    return this.devices;
   }
 
   overview() {
-    let devices = this.scan();
-    devices.unshift(['I2C Address', 'Description']);
-    console.log(DeviceListingGenerator.generate(devices));
+    return DeviceListingGenerator.generate(this.devices);
   }
 }
 
